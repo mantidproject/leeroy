08da812ea28546b9ca53e2cc427989f56cce3580
MialLewis <mial.lewis@stfc.ac.uk>
2023-09-13 16:46:24 +0100

    convert int to string

--- jenkins/jenkins.go
+++ jenkins/jenkins.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"encoding/xml"
 	"io/ioutil"
+	"strconv"
 )
 
 type Client struct {
@@ -117,7 +118,7 @@ func (c *Client) BuildWithParameters(job string, parameters string) error {
 
 func (c *Client) GetJobInstance(job string, pr_number int, sha string) (int, error) {
 	// set up the request
-	url := fmt.Sprintf("%s/job/%s/api/xml?tree=builds[number,result,actions[parameters[name,value]]]&xpath=/freeStyleProject/build[action/parameter[name=\"PR\"][value=\"%s\"]][action/parameter[name=\"GIT_SHA1\"][value=\"%s\"]][not(result)]&wrapper=found_jobs", c.Baseurl, job, pr_number, sha)
+	url := fmt.Sprintf("%s/job/%s/api/xml?tree=builds[number,result,actions[parameters[name,value]]]&xpath=/freeStyleProject/build[action/parameter[name=\"PR\"][value=\"%s\"]][action/parameter[name=\"GIT_SHA1\"][value=\"%s\"]][not(result)]&wrapper=found_jobs", c.Baseurl, job, strconv.Itoa(pr_number), sha)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return 0, err
